277667db9f1cb142b4afd8b7acaefc65cc8bb2d7
Poorvansha123 <balpoorva@gmail.com>
2026-08-08 12:13:02 +0530

    authentication through heade and cookies both

--- controllers/user.js
+++ controllers/user.js
@@ -19,9 +19,11 @@ async function handleUserLogin(req,res){
     if(!user) return res.render("login",{
         error:"Invalid Username or password",
     })
-    const sessionId=uuidv4();
-    setUser(sessionId,user)
-    res.cookie("uid",sessionId)
+   
+  const token= setUser(user)
+    res.cookie("uid",token,{
+        domain:"www.google.com",
+    })
 return res.redirect("/")
 }
 
